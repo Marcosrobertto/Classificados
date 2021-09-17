01c83f4fa9cdcfe13d8761c654f054304b1d01c7
Marcos Roberto <marcosrobertto46@gmail.com>
2021-09-17 13:13:06 -0300

    Classificados Front

---
+++ classificados/src/Pages/Home/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Page Home</h1>
+
+        <a href="/Login">Login</a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ classificados/src/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+
+import './index.css';
+
+import App from './Pages/Home/App';
+import Login from './Pages/Login/Login';
+
+import reportWebVitals from './reportWebVitals';
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} /> {/* Home*/}
+        <Route path="/login" component={Login} /> {/* Login*/}
+      </Switch>
+    </div>
+  </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
